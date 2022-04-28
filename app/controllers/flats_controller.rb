9da2983341857a47e57be3e82d9018540bdfcd97
Felipe Zica <felipevzica@gmail.com>
2022-04-28 19:39:59 -0300

    edit and update view and controller

--- app/controllers/flats_controller.rb
+++ app/controllers/flats_controller.rb
@@ -1,5 +1,5 @@
 class FlatsController < ApplicationController
-
+  before_action :find, only: [:show, :edit, :update, :destroy]
   def new
     @flat = Flat.new
   end
@@ -17,8 +17,23 @@ class FlatsController < ApplicationController
     end
   end
 
+  def update
+    @flat = Flat.find(params[:id])
+    if @flat.update(flat_params)
+      redirect_to flats_path
+    else
+      render :edit
+    end
+  end
+
   def show
     @flat = Flat.find(params[:id])
+  end
+
+  def destroy
+    @flat = Flat.find(params[:id])
+    @flat.destroy
+    redirect_to flats_path
   end
 
 private
@@ -27,6 +42,9 @@ private
     params.require(:flat).permit(:name, :address, :description, :price_per_night, :number_of_guests)
   end
 
+   def find
+    @flat = Flat.find(params[:id])
+  end
 
 
 end
